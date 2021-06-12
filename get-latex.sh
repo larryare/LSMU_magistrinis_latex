aaa789a99ef1bb00c37574b1916ac21fd8db7f40
Laurynas Kerežius <laurynas@kerezius.lt>
2021-06-12 19:46:24 +0300

    🔥 remove mano.tex, fix typos

--- get-latex.sh
+++ get-latex.sh
@@ -1,8 +1,10 @@
 echo "Diegiamas LaTeX (PdfLaTeX, XeTeX ir kt.)"
-sudo get install texlive-full
+sudo apt install texlive-full
 echo "Diegiama literatūros sąrašo tvarkyklė Biber skirta BibLaTeX paketui"
-sudo get install biber
+sudo apt install biber
 echo "Diegiami OpenType šriftai"
-sudo get install fonts-texgyre
+sudo apt install fonts-texgyre
 echo "Diegiamas Palemonas šriftas į sistemą"
 sudo cp -r Palemonas-2.1 /usr/share/fonts/truetype/
+echo "Kopijuojamas magistrinis.tex į mano.tex"
+cp magistrinis.tex mano.tex
